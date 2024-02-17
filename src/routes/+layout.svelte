2a74619624e2f4f515356f155db64d36e85fb0d9
Stéphane Payrard <cognominal@gmail.com>
2024-02-17 03:28:06 +0100

    move Navbar to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
     import "../app.css";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, ImagePlaceholder, Skeleton, TextPlaceholder } from 'flowbite-svelte';
+
   </script>
   
+  <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
+    <NavBrand href="/">
+      <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+      <NavLi href="/" active={true}>Home</NavLi>
+      <NavLi href="/About">About</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+  </Navbar>
+
   <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,20 +9,6 @@
   </script>
   
   <div class="relative px-8">
-    <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
-      <NavBrand href="/">
-        <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
-      </NavBrand>
-      <NavHamburger />
-      <NavUl>
-        <NavLi href="/" active={true}>Home</NavLi>
-        <NavLi href="/About">About</NavLi>
-        <NavLi href="/docs/components/navbar">Navbar</NavLi>
-        <NavLi href="/pricing">Pricing</NavLi>
-        <NavLi href="/contact">Contact</NavLi>
-      </NavUl>
-    </Navbar>
     <div style="height:300px;" class="overflow-scroll pb-16">
       <Skeleton class="mt-16 mb-8" />
       <ImagePlaceholder class="my-8" />
