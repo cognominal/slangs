7d5b99476f5bbe404d374c8b3824ca529f5367c7
Stéphane Payrard <cognominal@gmail.com>
2024-02-19 01:48:46 +0100

    [PTB]  more work including tab styling
    Need to restore the crumbs

--- src/lib/ParsetreeBrowser.svelte
+++ src/lib/ParsetreeBrowser.svelte
@@ -4,10 +4,12 @@
 	import { onMount } from 'svelte';
 	import { parse } from 'yaml';
 	import { calcCrumbs, type Crumbs, type ParseTree } from '$lib/utils';
+	import { Tabs, TabItem, Timeline, TimelineItem, Button, Hr } from 'flowbite-svelte';
 	export let parsedText: string;
 	export let parseTreeText: string;
 	let parseTree: object;
 	let error = 'no error';
+	let rulesTxt = 'will display the code of matched rules';
 	let crumbs: Crumbs;
 	try {
 		parseTree = parse(parseTreeText);
@@ -38,9 +40,35 @@
 	{/each}
 </Navbar> -->
 
-<div class="full-height">
+<Hr />
+<div class="border border-gray-400 hover:border-gray-900">
 	<Splitpanes horizontal={false}>
 		<Pane minSize={15}><CodeMirror bind:value={parsedText} /></Pane>
-		<Pane><CodeMirror bind:value={parseTreeText} /></Pane>
+		<Pane>
+			<Tabs contentClass="bg-gray-50  dark:bg-gray-800">
+				<TabItem open title="parsetree">
+					<CodeMirror bind:value={parseTreeText} />
+				</TabItem>
+				<TabItem title="rules">
+					<CodeMirror bind:value={rulesTxt} />
+				</TabItem>
+			</Tabs>
+		</Pane>
 	</Splitpanes>
 </div>
+<!-- <div class="full-height">
+	<Splitpanes horizontal={false}>
+		<Pane minSize={15}><CodeMirror bind:value={parsedText} /></Pane>
+		<Tabs>
+			<TabItem open>
+				<span slot="title">Rules</span>
+				<Pane><CodeMirror bind:value={parseTreeText} /></Pane>
+			</TabItem>
+			<TabItem>
+				<span slot="title">Parsetree</span>
+
+				<Pane><CodeMirror bind:value={parseTreeText} /></Pane>
+			</TabItem>
+		</Tabs>
+	</Splitpanes>
+</div> -->
