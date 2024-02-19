d9559d77c9486dd3871b849e53a82ed5aa54c6ed
Stéphane Payrard <cognominal@gmail.com>
2024-02-19 01:38:47 +0100

    Add a favicon and .svg logo
    A tree because the site theme is a parsetree browser.
    Probably a more distinctive recognizable logo when displayed  as favicon would be better. Also the current favicon is heavy. But that's a start.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 
 <Navbar class="px-2 sm:px-4 py-2.5 w-full z-20 top-0 start-0 border-b">
 	<NavBrand href="/">
-		<!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+		<img src="favicon.png" class="me-3 h-6 sm:h-9" alt="Slangs Logo" />
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Slangs</span>
 	</NavBrand>
 	<NavHamburger />
@@ -23,11 +23,8 @@
 		<NavLi href="/About">About</NavLi>
 		<NavLi href="/Playground">PlayGround</NavLi>
 		<NavLi href="https://github.com/cognominal/slangs">
-			<img src="github-mark/github-mark.svg" class="me-3 h-3 sm:h-6" alt="Flowbite Logo" />
+			<img src="github-mark/github-mark.svg" class="me-3 h-3 sm:h-6" alt="Slangs Logo" />
 		</NavLi>
-		<!-- <NavLi href="/docs/components/navbar">Navbar</NavLi> -->
-		<!-- <NavLi href="/pricing">Pricing</NavLi>
-      <NavLi href="/contact">Contact</NavLi> -->
 	</NavUl>
 </Navbar>
 <div class="relative px-8">
