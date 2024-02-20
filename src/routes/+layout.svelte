891a2ff8f463f5e6fb29bd509b4f77360fc5ec32
Stéphane Payrard <cognominal@gmail.com>
2024-02-20 02:36:12 +0100

    [content] add site entries to be filled

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,6 @@
 <script>
 	import '../app.css';
-	import {
-		Navbar,
-		NavBrand,
-		NavLi,
-		NavUl,
-		NavHamburger,
-		ImagePlaceholder,
-		Skeleton,
-		TextPlaceholder
-	} from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
 <Navbar class="px-2 sm:px-4 py-2.5 w-full z-20 top-0 start-0 border-b">
@@ -20,8 +11,10 @@
 	<NavHamburger />
 	<NavUl>
 		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi href="/Blog">Blog</NavLi>
+		<NavLi href="/Glossary">Glossary</NavLi>
 		<NavLi href="/About">About</NavLi>
-		<NavLi href="/Playground">PlayGround</NavLi>
+		<NavLi href="/Playground">Playground</NavLi>
 		<NavLi href="https://github.com/cognominal/slangs">
 			<img src="github-mark/github-mark.svg" class="me-3 h-3 sm:h-6" alt="Slangs Logo" />
 		</NavLi>
